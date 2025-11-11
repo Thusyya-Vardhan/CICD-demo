ea6186b9bbe8a774ac7313575b59f047cfe9c87d
Thusyya-Vardhan <thusyya2006@gmail.com>
2025-11-11 20:33:36 +0530

    New code for 11.py

--- 11.py
+++ 11.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-driver = webdriver.Chrome()
+driver=webdriver.Chrome()
 
-driver.get("http://google.com")
+driver.get("http://bing.com")
 
-print("Title:",driver.title)
+print("Opened Page:",driver.title)
 
-driver.quit()
+search=driver.find_element(By.NAME,'q')
+search.send_keys("selenium tutorial")
+search.send_keys(Keys.RETURN)
+
+time.sleep(3)
+
+print("Opened page:",driver.title)
+
+if "selenium" in driver.title:
+    print("Test passed")
+else:
+    print("Test failed")
